adf74118f2bc74e45246c32f3024e5d943ee559e
Eric McKinney <mckinney99@gmail.com>
2018-06-12 20:58:06 -0500

    simple app for iOS 101 course

--- About Me/ViewController.swift
+++ About Me/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var infoView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func introduceMe(_ sender: UIButton) {
+        let name = "Eric"
+        let hometown = "Burlington"
+        let color = "red"
+        let food = "pizza"
 
+        print("name: \(name)")
+        print("hometown: \(hometown)")
+        print("color: \(color)")
+        print("food: \(food)")
+
+        infoView.text = "Name: \(name)\n Hometown: \(hometown)\n Favorite Color: \(color)\n Favorite Food: \(food)"
+        
+    }
 }
 
